4f1128f9c93a52ddad5e729505e94c873dd0f76e
Kelly <kellybrown777@gmail.com>
2025-11-24 02:15:58 -0500

    fixing variable redundancy I didn't notice at first

--- generate-test-data/generate-author-list.py
+++ generate-test-data/generate-author-list.py
@@ -6,14 +6,12 @@ def generate_author_list():
 
     counter = 0
     total_authors = 55
-    author_combined_list = []
     while counter < total_authors:
         counter = counter + 1
         author_combined_name = f"(\"fname{counter}\"), (\"lname{counter}\"),"
-        author_combined_list.append(author_combined_name)
         author_list.append(author_combined_name)
     with open('./files/author_list.csv', 'w') as f:
-        for i in author_combined_list:
+        for i in author_list:
             f.write(f"{i}\n")
 
 
