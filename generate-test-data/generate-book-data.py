4e5fcc77e0cc252b41826c98b28e7d10cdc8cf1c
Kelly <kellybrown777@gmail.com>
2025-12-16 01:25:50 -0500

    updates for many to many relationship between books and authors to account for BOTH authors with multiple books AND books with multiple authors

--- generate-test-data/generate-book-data.py
+++ generate-test-data/generate-book-data.py
@@ -10,18 +10,17 @@ def generate_book_list():
         total_books = 86
         book_list = []
         with open('files/book_list.csv', 'w') as f:
-            f.write("title,author_id,genre_id,pages,\n")
+            f.write("title,genre_id,pages,status_id\n")
             while total_books > counter:
                 counter = counter + 1
                 book_title = f"Title {counter}"
                 book_list.append(book_title)
             for i in book_list:
-                author = random.randint(1, 55)
                 genre = random.randint(1, 22)
                 pages = random.randint(81, 1034)
                 status_choices = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 4, 4, 4, 2, 2, 2, 4, 4, 2, 5, 2, 4, 4, 4]
                 status = random.choice(status_choices)
-                f.write(f"('{i}', {author}, {genre}, {pages}, {status}),\n")
+                f.write(f"('{i}', {genre}, {pages}, {status}),\n")
 
 
 
